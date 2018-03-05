308a3fc298d55e713138a420b2b2920852b77073
Hanwei Liu <lhanwei.1993@gmail.com>
2018-03-05 21:52:28 +0000

    D3 data visualization practice: Global CO2 dashboard

---
+++ app.js
@@ -0,0 +1,116 @@
+//get data into js
+d3.queue()
+  .defer(d3.json, "//unpkg.com/world-atlas@1.1.4/world/50m.json")
+  .defer(d3.csv, "./data/all_data.csv", function(row) {
+    return {    //formatter
+      continent: row.Continent,
+      country: row.Country,
+      countryCode: row["Country Code"],
+      emissions: +row["Emissions"],
+      emissionsPerCapita: +row["Emissions Per Capita"],
+      region: row.Region,
+      year: +row.Year
+    }
+  })
+  
+  //get extreme values for the years in the dataset
+  .await(function(error, mapData, data) {
+    if (error) throw error;
+    
+    var extremeYears = d3.extent(data, d => d.year);
+    var currentYear = extremeYears[0];
+    var currentDataType = d3.select('input[name="data-type"]:checked')
+                            .attr("value");
+    var geoData = topojson.feature(mapData, mapData.objects.countries).features;
+
+    var width = +d3.select(".chart-container")
+                   .node().offsetWidth;
+    var height = 300;
+
+    //Create charts
+    createMap(width, width * 4 / 5);
+    createPie(width, height);
+    createBar(width, height);
+    drawMap(geoData, data, currentYear, currentDataType);
+    drawPie(data, currentYear);
+    drawBar(data, currentDataType, "");
+    
+    //get range 
+    d3.select("#year")
+        .property("min", currentYear)
+        .property("max", extremeYears[1])
+        .property("value", currentYear)
+        //value changes, get new current year and redo graph
+        .on("input", () => {    
+          currentYear = +d3.event.target.value;
+          drawMap(geoData, data, currentYear, currentDataType);
+          drawPie(data, currentYear);
+          highlightBars(currentYear);
+        });
+    
+    //event lisender for the radio buttons
+    d3.selectAll('input[name="data-type"]')
+        .on("change", () => {
+          var active = d3.select(".active").data()[0];
+          var country = active ? active.properties.country : "";
+          currentDataType = d3.event.target.value;
+          drawMap(geoData, data, currentYear, currentDataType);
+          drawBar(data, currentDataType, country);
+        });
+
+    d3.selectAll("svg")
+        .on("mousemove touchmove", updateTooltip);
+    
+    //tooltip
+    function updateTooltip() {
+      //turn a hover to a d3 selection
+      var tooltip = d3.select(".tooltip");
+      var tgt = d3.select(d3.event.target);
+      //find out where the event come from
+      var isCountry = tgt.classed("country");
+      var isBar = tgt.classed("bar");
+      var isArc = tgt.classed("arc");
+      var dataType = d3.select("input:checked")
+                       .property("value");
+      var units = dataType === "emissions" ? "thousand metric tons" : "metric tons per capita";
+      var data;
+      var percentage = "";
+      
+      if (isCountry) data = tgt.data()[0].properties;
+      
+      if (isArc) {
+        data = tgt.data()[0].data;
+        percentage = `<p>Percentage of total: ${getPercentage(tgt.data()[0])}</p>`;
+      }
+      
+      if (isBar) data = tgt.data()[0];
+      tooltip
+          .style("opacity", +(isCountry || isArc || isBar))
+          .style("left", (d3.event.pageX - tooltip.node().offsetWidth / 2) + "px")
+          .style("top", (d3.event.pageY - tooltip.node().offsetHeight - 10) + "px");
+          
+      if (data) {
+        var dataValue = data[dataType] ?
+          data[dataType].toLocaleString() + " " + units :
+          "Data Not Available";
+        tooltip 
+            .html(`
+              <p>Country: ${data.country}</p>
+              <p>${formatDataType(dataType)}: ${dataValue}</p>
+              <p>Year: ${data.year || d3.select("#year").property("value")}</p>
+              ${percentage}
+            `)
+      }
+    }
+  });
+
+function formatDataType(key) {
+  return key[0].toUpperCase() + key.slice(1).replace(/[A-Z]/g, c => " " + c);
+}
+
+//calculate percentage: calc diff between those two angles, then divide by 2*pi and * 100 (between (0,100)
+function getPercentage(d) {
+  var angle = d.endAngle - d.startAngle;
+  var fraction = 100 * angle / (Math.PI * 2);
+  return fraction.toFixed(2) + "%";
+}
